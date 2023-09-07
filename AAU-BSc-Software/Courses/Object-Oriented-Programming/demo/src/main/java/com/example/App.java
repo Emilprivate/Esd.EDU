3034caf6f43f1baa6c6e7ac3da89ecbceb3160e4
Emil D <global52600894@gmail.com>
2023-09-07 02:01:06 +0200

    Java, yay, OOP

---
+++ AAU-BSc-Software/Courses/Object-Oriented-Programming/demo/src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        while (1 == 1) {
+            System.out.println("aaaee aaaaaa eeeeee aaaaaa eeeeee");
+        }
+    }
+}
